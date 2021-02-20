ad685ec12ded33a1c3f95dabc16038623acb04bc
maste <maste@192.168.0.11>
2021-02-20 12:23:27 -0600

    weweraert

--- SecondOne/src/SecondOne.java
+++ SecondOne/src/SecondOne.java
@@ -3,6 +3,8 @@ public class SecondOne {
 
 	public static void main(String[] args) {
 		System.out.println("Slam 3LAYKOM WA RHMAT ALAH");
+		System.out.println("second commit");
+		System.out.println("ryryy");
 
 	}
 
